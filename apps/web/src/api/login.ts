718859e433459a870cebbd351e1759221958bc2e
subikstha <subsatch@gmail.com>
2026-08-15 17:16:22 +0545

    feat: login form

---
+++ apps/web/src/api/login.ts
@@ -0,0 +1,14 @@
+export default async function login(email: string, password: string) {
+  const response = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+}
--- apps/web/src/components/forms/Login.tsx
+++ apps/web/src/components/forms/Login.tsx
@@ -1,5 +1,35 @@
+import { useMutation } from "@tanstack/react-query";
+import type React from "react";
+import login from "../../api/login";
 const Login = () => {
-  return <form></form>;
+  const mutation = useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      login(email, password),
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+    if (typeof email !== "string" || typeof password !== "string") return;
+
+    mutation.mutate({
+      email,
+      password,
+    });
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
