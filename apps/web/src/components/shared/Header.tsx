d18195042d4e7726667d7a8cc3eeaec80c90f848
Subik Shrestha <sushr926@kathmandu>
2026-09-04 12:27:34 +0545

    feat: installed ant design and updated the header component

--- apps/web/src/components/shared/Header.tsx
+++ apps/web/src/components/shared/Header.tsx
@@ -1,5 +1,68 @@
-const Header = () => {
-  return <div>Header</div>;
+import { Layout, Flex, Menu, Dropdown, Avatar } from 'antd'
+import useAuth from '../../hooks/useAuth';
+import { Link } from '@tanstack/react-router';
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import { logout } from '../../api/auth';
+const { Header } = Layout
+
+const userItems = [
+  {
+    key: 'Profile',
+    label: 'profile'
+  },
+  {
+    key: 'logout',
+    label: 'Logout'
+  }
+]
+
+
+const AppHeader = () => {
+  const queryClient = new QueryClient()
+  const { isAuthenticated, isLoading, user } = useAuth();
+  const logoutMutation = useMutation({
+    mutationFn: () => logout(),
+    onSuccess: () => {
+      console.log("Logout success");
+      queryClient.invalidateQueries({
+        queryKey: ["me"],
+      });
+    },
+  });
+  function handleClick({ key }: { key: string }) {
+    switch (key) {
+      case 'profile':
+        console.log('user clicked on profile')
+        break;
+
+      case 'logout':
+        console.log('User clicked on logout')
+        logoutMutation.mutate()
+        break;
+    }
+  }
+
+
+  return <Header>
+    <Flex justify="space-between" align="center">
+      <div>My App</div>
+
+      <Menu
+        mode="horizontal"
+        items={[
+          { key: "home", label: "Home" },
+          { key: "tasks", label: "Tasks" },
+        ]}
+      />
+      {isAuthenticated ? (<Dropdown className="cursor-pointer" menu={{
+        items: userItems,
+        onClick: handleClick
+      }}>
+        <Avatar>{user?.email.split('')[0]}</Avatar>
+      </Dropdown>) : (<Link to="/login">Login</Link>)}
+
+    </Flex>
+  </Header>;
 };
 
-export default Header;
+export default AppHeader;
